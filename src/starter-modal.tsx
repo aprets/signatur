f7611cc52a505242551484ec1bde8e778d5ae701
aprets <15954684+aprets@users.noreply.github.com>
2026-08-20 03:20:32 +0100

    Use a neutral modal border

--- src/starter-modal.tsx
+++ src/starter-modal.tsx
@@ -438,7 +438,7 @@ const StarterModal = ({
       onRequestClose={closeModal}
       contentLabel="Welcome Modal"
       overlayClassName="fixed inset-0 bg-slate-800 bg-opacity-75 transition-opacity duration-500 opacity-0"
-      className="absolute left-1/2 top-1/2 isolate flex max-h-[95vh] min-h-[28rem] w-[90vw] -translate-x-1/2 -translate-y-1/2 transform overflow-auto rounded-2xl bg-violet-700 shadow-2xl outline-none ring-1 ring-black/10 lg:w-[54rem]"
+      className="absolute left-1/2 top-1/2 isolate flex max-h-[95vh] min-h-[28rem] w-[90vw] -translate-x-1/2 -translate-y-1/2 transform overflow-auto rounded-2xl border border-black/10 bg-violet-700 bg-clip-padding shadow-2xl outline-none lg:w-[54rem]"
       closeTimeoutMS={500}
     >
       <aside className="flex w-56 shrink-0 flex-col bg-violet-700 lg:w-64">
